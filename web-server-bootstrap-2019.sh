93f42a501ec18a302fb1093982bfb74682d1c7ae
Anh K <ahoang10@jhu.edu>
2019-05-22 02:06:48 -0400

    Update web-server-bootstrap-2019.sh

--- web-server-bootstrap-2019.sh
+++ web-server-bootstrap-2019.sh
@@ -2,7 +2,9 @@
 yum update -y
 yum install httpd -y
 service httpd start
+chkconfig httpd on
 cd /var/www/html
 echo "<html><h1>Hi this is a bootstraped web server </h1></html>" > index.html
-aws s3 mb s3://anhhoangtestbucketohio2019
-aws s3 cp indext.html s3://anhhoangtestbucketohio2019
+aws s3 mb s3://ANYOTHERUNIQUENAME
+aws s3 cp index.html s3://ANYOTHERUNIQUENAME
+
